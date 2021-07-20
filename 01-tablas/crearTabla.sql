4350a9d7f8ba3abfd875b8255a44fb7533232904
Yann <metalyanni@gmail.com>
2021-07-20 10:53:27 -0500

    Create Table

--- 01-tablas/crearTabla.sql
+++ 01-tablas/crearTabla.sql
@@ -2,5 +2,11 @@
 
 /* Creando una tabla */
 CREATE TABLE USUARIO(
-    
+    id INT AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL, 
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_usuario PRIMARY KEY (id)
 );
+

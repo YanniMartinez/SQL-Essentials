ce6067f0a0552e4ba51eeb2559cecac27e72f8e0
Yann <metalyanni@gmail.com>
2021-07-21 12:58:54 -0500

    Query with GROUP BY and HAVING

--- 05-consultaXCategoria/consultas.sql
+++ 05-consultaXCategoria/consultas.sql
@@ -6,3 +6,17 @@ SELECT COUNT(titulo), categoria_id FROM entrada GROUP BY categoria_id;
 
 /* AS significa un alias temporal mientras dura la consulta */
 SELECT COUNT(titulo) AS 'Número de entradas', categoria_id FROM entrada GROUP BY categoria_id;
+
+/*En group by no podemos usar el WHERE, pero si el Having, en este contexto sustituye al 
+Where */
+/* Consulta agrupada con condiciones */
+SELECT COUNT(titulo) AS 'Número de entradas', categoria_id 
+FROM entrada GROUP BY categoria_id
+HAVING COUNT(titulo) >= 4;
+/* En este ejemplo nos devuelve las entradas que tengan un contador mayor o igual a 4.
+Se lee: 
+Trae los titulos y cuentalos y la categoria_id de la tabla entradas agrupadas por categoria id
+cuando el número de entradas sea mayor o igual a 4
+ */
+
+ 

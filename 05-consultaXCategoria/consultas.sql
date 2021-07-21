af4142deaa1311af08af188e73741acfa801333c
Yann <metalyanni@gmail.com>
2021-07-21 12:51:37 -0500

    Temporal Name with AS, is an Alias

--- 05-consultaXCategoria/consultas.sql
+++ 05-consultaXCategoria/consultas.sql
@@ -3,3 +3,6 @@ Count genera un conteo */
 SELECT COUNT(titulo), categoria_id FROM entrada
 /* La siguiente consulta permite contar el número de entradas que hay según esa categoria */
 SELECT COUNT(titulo), categoria_id FROM entrada GROUP BY categoria_id;
+
+/* AS significa un alias temporal mientras dura la consulta */
+SELECT COUNT(titulo) AS 'Número de entradas', categoria_id FROM entrada GROUP BY categoria_id;
